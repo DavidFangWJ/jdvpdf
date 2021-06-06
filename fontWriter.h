02d201c50630e34309847a7c8cb20d7323aaa917
Xin-Shiyu <xinshiyu@188.com>
2021-06-06 21:31:11 +0800

    补上头文件包含

--- fontWriter.h
+++ fontWriter.h
@@ -5,6 +5,9 @@
 #ifndef JDVPDF_FONTWRITER_H
 #define JDVPDF_FONTWRITER_H
 
+#include "stdint.h"
+#include "fontObject.h"
+
 void outputSubsetCFF(size_t, uint16_t*, Font*);
 void outputSubsetSFNT(size_t, uint16_t*, Font*);
 
